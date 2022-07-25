cb97622ca0303f09b32e2b30dacc2a34daf134a5
Quimzy <bam.boy2020@gmail.com>
2022-07-25 22:16:30 +0300

    signup with facial recognition

--- api.py
+++ api.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+import sys
+sys.path.insert(1, 'nanolock/')
+
+from flask import Flask, render_template, request
+from nanolock.recognizer import Recognizer
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
 	return render_template("index.html")
+
+@app.route("/signup", methods=["POST"])
+def signup():
+	username = request.form["username"]
+	Recognizer(username).setup()
+	return "Done!"
 
 app.run()
