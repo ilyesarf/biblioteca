42d2726c23503afb848454668a28c322e842a94e
Quimzy <bam.boy2020@gmail.com>
2022-11-06 20:50:16 +0300

    works on phones toogit add .

--- web.py
+++ web.py
@@ -1,8 +1,8 @@
-import sys, os, json, shutil
+import sys, os, json
 sys.path.insert(1, 'nanolock/')
 
-from flask import Flask, render_template, request
-from hashlib import md5
+from flask import Flask, render_template, request, redirect
+
 from nanolock.recognizer import Verification
 from nanolock.recognizer import NoFaceDetected
 
@@ -11,11 +11,11 @@ app = Flask(__name__)
 
 verifier = Verification()
 
-def check_user(username):
-	if os.path.isfile("usernames.json"):
-		with open("usernames.json", "r") as f:
-			usernames = json.loads(f.read())
-			if username in usernames["usernames"]:
+def check_user(user_hash):
+	if os.path.isfile("users.json"):
+		with open("users.json", "r") as f:
+			user_hashes = json.loads(f.read())["user_hashes"]
+			if user_hash in user_hashes:
 				return True
 
 	return False
@@ -29,77 +29,80 @@ def signup():
 	error = None
 
 	if request.method == "POST":
-		username = request.form["username"]
+		user_hash = request.form["user_hash"]
+		b64enc_img = request.form["b64enc_img"]
 
-		if check_user(username) == False:
+		if check_user(user_hash) == False:
 			try:
-				verifier.setup(username)
-				return render_template("login.html")
+				verifier.add_user(user_hash, b64enc_img)
+
+				return redirect("/login")
 			except NoFaceDetected:
 				error="No Face Was Detected"
 				
-				verifier.usernames["usernames"].remove(username)
-				json.dump(verifier.usernames, open("usernames.json", "w"))
+				verifier.users["user_hashes"].remove(user_hash)
+				json.dump(verifier.users, open("users.json", "w"))
 
-				shutil.rmtree(f"nanolock/dataset/{md5(username.encode()).hexdigest()}")
+				os.remove(f"{verifier.dataset_path}{user_hash}.png")
 
 		else:
-			return render_template("login.html")
+			return redirect("/login")
 
 	return render_template('signup.html', error=error)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
 	error = None
 	
 	if request.method == "POST":
-		username = request.form["username"]
+		user_hash = request.form["user_hash"]
+		b64enc_img = request.form["b64enc_img"]
 
-		if check_user(username):
+		if check_user(user_hash):
 			accept_login = False
 			
 			try:
-				accept_login = verifier.accept_login(username)
+				accept_login = verifier.accept_login(user_hash, b64enc_img)
 
 				if accept_login:
-					return render_template("welcome.html", username=username)
+					return render_template("welcome.html")
 				else:
-					error = f"You are not {username} !!"
+					error = f"Face doesnt match !!"
 
 			except NoFaceDetected:
 				error = "Face not detected"
 		else:
-			error = f"User: {username} doesn't exist"
-
+			error = f"User doesn't exist"
+	
 	return render_template("login.html", error=error)
 
 @app.route("/delete_user", methods=["GET", "POST"])
 def delete_user():
-	usernames = json.loads(open("usernames.json", "r").read())
+	users = json.loads(open("users.json", "r").read())
 
 	error = None
 
 	if request.method == "POST":
-		username = request.form["username"]
-		if username == "Unknown":
-			error = "You cannot delete 'Unknown' user"
+		user_hash = request.form["user_hash"]
+		b64enc_img = request.form["b64enc_img"]
+		
+		if check_user(user_hash):
+			if verifier.accept_login(user_hash, b64enc_img):
 
-		if check_user(username):
-			if verifier.accept_login(username):
+				if len(users["user_hashes"]) == 1:
+					users["user_hashes"].remove(user_hash)
+					json.dump(users, open("users.json", "w"))
 
-				if len(usernames["usernames"]) == 2 and usernames["usernames"][0] == "Unknown":
-					usernames["usernames"].remove(username)
-					json.dump(usernames, open("usernames.json", "w"))
-
-					shutil.rmtree(f"nanolock/dataset/{md5(username.encode()).hexdigest()}")
+					os.remove(f"nanolock/dataset/{user_hash}.png")
 
 				else:
-					usernames["usernames"].remove(username)
-					json.dump(usernames, open("usernames.json", "w"))
-					verifier.usernames = usernames
+					users["users"].remove(user_hash)
+					json.dump(users, open("users.json", "w"))
+					verifier.users = users
 
-					shutil.rmtree(f"nanolock/dataset/{md5(username.encode()).hexdigest()}")
+					os.remove(f"nanolock/dataset/{user_hash}.png")
 
 				return render_template("index.html")
 			else:
@@ -110,4 +113,5 @@ def delete_user():
 
 	return render_template("delete_user.html", error=error)
 
-app.run(host='0.0.0.0', port="8080")
+if __name__ == '__main__':
+	app.run(host = "0.0.0.0", port="8080", ssl_context='adhoc', debug=True)
