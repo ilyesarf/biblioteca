f5a97b24c403485579d207b06c685061cb16bf92
Quimzy <bam.boy2020@gmail.com>
2022-11-24 21:24:09 +0300

    db: managing book store

--- db.py
+++ db.py
@@ -21,7 +21,7 @@ class DB:
 
     #Users
     def get_users(self):
-        query = "SELECT user_hash from users"
+        query = "SELECT user_hash FROM users"
         res = self.cur.execute(query)
         
         return res.fetchall()
@@ -32,7 +32,7 @@ class DB:
         self.con.commit()        
 
     def is_user(self, user_hash):
-        query = "SELECT user_hash from users WHERE user_hash=?"
+        query = "SELECT user_hash FROM users WHERE user_hash=?"
         res = self.cur.execute(query, (user_hash,))
         
         return len(res.fetchall()) > 0
@@ -42,3 +42,27 @@ class DB:
         self.cur.execute(query, (user_hash,))
         self.con.commit()
 
+    #Store
+    def get_books(self, user_hash):
+        query = "SELECT book_id, book_title, book_date FROM store WHERE user_hash=?"
+        res = self.cur.execute(query, (user_hash,))
+
+        return res.fetchall()
+    
+    def add_book(self, user_hash, book_id, book_title, book_date):
+        query = "INSERT INTO store VALUES (?, ?, ?, ?)"
+        self.cur.execute(query, (user_hash, book_id, book_title, book_date))
+        self.con.commit()
+    
+    def is_book(self, user_hash, book_id):
+        query = "SELECT book_id FROM store WHERE user_hash=? AND book_id=?"
+        res = self.cur.execute(query, (user_hash, book_id))
+
+        return len(res.fetchall()) > 0
+    
+    def delete_book(self, user_hash, book_id):
+        query = "DELETE FROM store WHERE user_hash=? AND book_id=?"
+        self.cur.execute(query, (user_hash, book_id))
+        self.con.commit()
+
+    
